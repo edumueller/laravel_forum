753441e9d65be12a9a67b7ae0a5914d5c40d2514
Eduardo <edu.nicastro@gmail.com>
2018-07-05 01:18:24 -0300

    Added the user name to the replies on the thread view

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -18,6 +18,9 @@
             <div class="col-md-8 col-md-offset-2">
                 @foreach ($thread->replies as $reply)
                 <div class="card">
+                    <div class="card-header">
+                        {{ $reply->owner->name }} said {{ $reply->created_at->diffForHumans() }}
+                    </div>
                     <div class="card-body">
                         {{ $reply->body }}
                     </div>
