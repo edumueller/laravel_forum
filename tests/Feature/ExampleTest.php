4bdb4c29fa3d1ecee7f8173212fdeb4a17c32cec
Eduardo <edu.nicastro@gmail.com>
2018-07-05 00:38:44 -0300

    Added the test a_user_can_read_a_single_thread and renamed the other test to a_user_can_view_all_threads.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -8,12 +8,22 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class ExampleTest extends TestCase
 {
     use DatabaseMigrations;
+
     /** @test */
-    public function a_user_can_browse_threads()
+    public function a_user_can_view_all_threads()
     {
         $thread = factory('App\Thread')->create();
-        $response = $this->get('/threads');
 
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
         $response->assertSee($thread->title);
     }
 }
