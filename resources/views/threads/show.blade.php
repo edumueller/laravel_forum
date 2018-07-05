6465177c8c073e91956bd1d878c814b363ca43d2
Eduardo <edu.nicastro@gmail.com>
2018-07-05 01:06:07 -0300

    Added the replies to the view

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,5 +13,17 @@
                 </div>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @foreach ($thread->replies as $reply)
+                <div class="card">
+                    <div class="card-body">
+                        {{ $reply->body }}
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
     </div>
 @endsection
